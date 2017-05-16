c5cea9b0a5b80dc25a829cb7091c663617248c95
afolch <afolch@G2-04>
2017-05-16 13:47:54 +0200

    Created first Gerstner Wave, with frequency, amplitude and wave height variables interaction. Need to apply multiple waves and force with external objects.

--- src/physics.cpp
+++ src/physics.cpp
@@ -23,32 +23,32 @@ const int meshColumns = 20;
 const int totalVertex = meshRows * meshColumns;
 
 static float L = 0.5f;
-static int resetTime = 10;
+static int resetTime = 20;
 static float dtCounter = 0;
 static float height = 2.0f;
+static float amplitude = 0.3f;
+static float frequency = 3.5f;
+
 static int lastTime;
-static float lastHeight;
+static float lastHeight, lastAmplitude, lastFrequency;
 
 //Mesh arrays
 glm::vec3 *nodeVectors;
-glm::vec3 *lastVectors;
-glm::vec3 *velVectors;
-glm::vec3 *newVectors;
-glm::vec3 *forceVectors;
+glm::vec3 *initalVector;
+glm::vec3 waveVector = { 1,0,0 };
 
-//Cube planes
-glm::vec3 groundN = { 0,1,0 };
-glm::vec3 roofN = { 0,-1,0 };
-glm::vec3 leftN = { 1,0,0 };
-glm::vec3 rightN = { -1,0,0 };
-glm::vec3 backN = { 0,0,1 };
-glm::vec3 frontN = { 0,0,-1 };
 
 void GUI() {
 
 	ImGui::Text("Application average %.3f ms/frame (%.1f FPS)", 1000.0f / ImGui::GetIO().Framerate, ImGui::GetIO().Framerate);
+	ImGui::Spacing(); ImGui::Spacing(); ImGui::Spacing(); ImGui::Spacing();
 	ImGui::SliderInt("Reset Time", &resetTime, 0, 20);
+	ImGui::Spacing(); ImGui::Spacing(); ImGui::Spacing(); ImGui::Spacing();
 	ImGui::SliderFloat("Mesh height", &height, 0.1f, 9.9f);
+	ImGui::Spacing(); ImGui::Spacing(); ImGui::Spacing(); ImGui::Spacing();
+	ImGui::SliderFloat("Amplitude", &amplitude, 0.1f, 0.9f);
+	ImGui::Spacing(); ImGui::Spacing(); ImGui::Spacing(); ImGui::Spacing();
+	ImGui::SliderFloat("Frequency", &frequency, 0.1f, 5.f);
 
 	if (show_test_window) {
 		ImGui::SetNextWindowPos(ImVec2(650, 20), ImGuiSetCond_FirstUseEver);
@@ -56,52 +56,24 @@ void GUI() {
 	}
 }
 
-void reset() {
-
-	//Function that resets to the beggining all the positions, forces and velocites of the mesh
-	int columnsCounter = 0;
-	int rowsCounter = 0;
-	lastTime = resetTime;
-	lastHeight = height;
-
-	for (int i = 0; i < totalVertex; i++) {
-		nodeVectors[i] = { L * columnsCounter - (L*meshColumns / 2) + L / 2,height, L * rowsCounter - (L*meshRows / 2) + L / 2 };
-		velVectors[i] = { 0,0,0 };
-		newVectors[i] = nodeVectors[i];
-		forceVectors[i] = { 0,0,0 };
-		if (columnsCounter >= meshColumns - 1) {
-			columnsCounter = 0;
-			rowsCounter += 1;
-		}
-		else { columnsCounter += 1; }
-	}
-}
-
-void checkChanges() { if (lastHeight != height || lastTime != resetTime) reset(); }
-
 void PhysicsInit() {
 
 	//Creation of all glm::vec3 arrays
 	nodeVectors = new glm::vec3[totalVertex];
-	velVectors = new glm::vec3[totalVertex];
-	newVectors = new glm::vec3[totalVertex];
-	forceVectors = new glm::vec3[totalVertex];
-	lastVectors = new glm::vec3[totalVertex];
+	initalVector = new glm::vec3[totalVertex];
 
 	//Declaration of mesh counters
 	int columnsCounter = 0;
 	int rowsCounter = 0;
 
 	//Applying values to "last" variables for reseting
-	lastTime = resetTime;
-	lastHeight = height;
+	lastTime = resetTime;	lastHeight = height;	lastAmplitude = amplitude;	lastFrequency = lastFrequency;
 
 	//For that creates the Mesh with an "L" separation
-	for (int i = 0; i < totalVertex; i++) {
+	for (int i = 0; i < totalVertex; i++) { 
 		nodeVectors[i] = { L * columnsCounter - (L*meshColumns / 2) + L / 2,height, L * rowsCounter - (L*meshRows / 2) + L / 2 };
-		velVectors[i] = { 0,0,0 };
-		newVectors[i] = nodeVectors[i];
-		forceVectors[i] = { 0,0,0 };
+		initalVector[i] = nodeVectors[i];
+
 		if (columnsCounter >= meshColumns - 1) {
 			columnsCounter = 0;
 			rowsCounter += 1;
@@ -110,33 +82,24 @@ void PhysicsInit() {
 	}
 }
 
+void checkChanges() { if (lastHeight != height || lastTime != resetTime || lastFrequency != frequency || lastAmplitude != amplitude) PhysicsInit(); }
 
 void PhysicsUpdate(float dt) {
-
-	for (int i = 0; i < totalVertex; i++) { //Applying Euler's solver and updating
-
-		lastVectors[i] = nodeVectors[i]; //Store last position vector
-
-		//Velocities and gravity
-		velVectors[i].x = velVectors[i].x + dt * forceVectors[i].x;
-		velVectors[i].z = velVectors[i].z + dt * forceVectors[i].z;
-
-		forceVectors[i] = glm::vec3(0, 0, 0); //Reset forces
-		
-		newVectors[i] = nodeVectors[i] + dt * velVectors[i]; //Euler 
-
-		nodeVectors[i] = newVectors[i]; //Update position
-
-		}
 	
-	checkChanges();
 	dtCounter += dt;
+	checkChanges();
 
-	if (dtCounter >= resetTime) { reset(); dtCounter = 0; } //Reset every "x" seconds
+	for (int i = 0; i < totalVertex; i++) { //Applying Gerstner waves
+
+		nodeVectors[i] = initalVector[i] - (waveVector /glm::length(waveVector)) * amplitude * sin(glm::dot(waveVector,initalVector[i]) - frequency * dtCounter); //Calculation of Gernster Waves 'X'
+		nodeVectors[i].y = amplitude * cos(glm::dot(waveVector, initalVector[i]) - frequency * dtCounter) + height; //Calculation of Gernster Waves 'Y'
+		
+	}
+	
+	if (dtCounter >= resetTime) {PhysicsInit(); dtCounter = 0; } //Reset every "x" seconds
 
 	ClothMesh::updateClothMesh(&nodeVectors[0].x);
 
 }
-
 
 void PhysicsCleanup() {}
