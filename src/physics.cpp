f31acb01b64776ede101b20dd03ae6d00c7a87a3
afolch <afolch@G2-04>
2017-05-24 12:01:02 +0200

    Buoyancy almost done, need to fix the height and the closest nodes so it applies correctly.

--- src/physics.cpp
+++ src/physics.cpp
@@ -7,6 +7,7 @@
 #include <iostream>
 #include <time.h>
 #include <math.h>
+#include <map>
 
 bool show_test_window = false;
 
@@ -56,9 +57,10 @@ static float mass = 1.0f;
 static float gravity = 9.81f;
 static float radius = 0.5f;
 static float density = 10.f;
-static float v_sub;
+static float v_sub, fluidHeight, sphereCorrectPosition;
 glm::vec3 y = { 0,1,0 };
-glm::vec3 sphereVector;
+glm::vec3 sphereVector, lastSphereVector, startingVector, buoyancy, sphereVelocity;
+float *closePos,sphereFluidDistance, sinkedVolume;
 
 void GUI() {
 
@@ -85,7 +87,7 @@ void GUI() {
 	ImGui::SliderFloat("Sphere mass", &mass, 0.01f, 10.f); ImGui::Spacing(); ImGui::Spacing(); ImGui::Spacing(); ImGui::Spacing();
 	ImGui::SliderFloat("Sphere density", &density, 0.1f, 20.f); ImGui::Spacing(); ImGui::Spacing(); ImGui::Spacing(); ImGui::Spacing();
 	ImGui::SliderFloat("Sphere radius", &radius, 0.1f, 10.f); ImGui::Spacing(); ImGui::Spacing(); ImGui::Spacing(); ImGui::Spacing();
-	ImGui::SliderFloat3("Sphere vector", &sphereVector.x, 0.5f, 6.f); ImGui::Spacing(); ImGui::Spacing(); ImGui::Spacing(); ImGui::Spacing();
+	ImGui::SliderFloat3("Sphere vector", &startingVector.x, 0.5f, 9.f); ImGui::Spacing(); ImGui::Spacing(); ImGui::Spacing(); ImGui::Spacing();
 
 	if (show_test_window) {
 		ImGui::SetNextWindowPos(ImVec2(650, 20), ImGuiSetCond_FirstUseEver);
@@ -93,12 +95,61 @@ void GUI() {
 	}
 }
 
+//Insertion sort algorithm to sort highest and lowest values of an array
+void insertionSort(float arr[], int length) {
+
+	int i, j, tmp;
+
+	for (i = 1; i < length; i++) {
+		j = i;
+		while (j > 0 && arr[j - 1] > arr[j]) {
+			tmp = arr[j];
+			arr[j] = arr[j - 1];
+			arr[j - 1] = tmp;
+			j--;
+		}
+	}
+}
+
+void getfluidHeight(int closestNodes) {
+
+	//Calculus of the nearest nodes to the sphere
+	for (int i = 0; i < totalVertex; i++) { closePos[i] = glm::distance(sphereVector.y,nodeVectors[i].y); } //Calculus of the distance between center of the sphere and nodes of the mesh
+
+	insertionSort(closePos, totalVertex); //Sort the distance array to access closest nodes values
+
+										  //Get the "x" most closest values
+	for (int i = totalVertex - 1; i > totalVertex - closestNodes - 1; i--) { fluidHeight += closePos[i]; }
+	fluidHeight = fluidHeight / closestNodes; //Generate an average value
+
+}
+
+void calculateBuoyancy(float dt) {
+
+	sphereCorrectPosition = radius - sphereVector.y; //Calculate distance between the sphere and the fluid
+	sphereFluidDistance = glm::distance(fluidHeight, sphereCorrectPosition);
+
+	//If the sphere is below the fluid apply buoyancy
+	std::cout << sphereFluidDistance << std::endl;
+	if (sphereFluidDistance < 0) {
+		float diameter = (radius * 2)*(radius * 2);
+		sinkedVolume = diameter*sphereFluidDistance; //Calculation of v_sub
+		buoyancy = (density * gravity * sinkedVolume) * y; //Total calculation of buoyancy 
+		sphereVelocity += (buoyancy/mass) * dt; //Apply buoyancy to sphere vector
+		std::cout << buoyancy.y << std::endl;
+	}
+
+}
+
+
 void initMultipleWaves() {
 
+	//Creation of all the waves arrays
 	waveVectorArray = new glm::vec3[totalWaves];
 	frequencyArray = new float[totalWaves];
 	amplitudeArray = new float[totalWaves];
 
+	//Applying values to "last" variables for reseting
 	lastTotalWaves = totalWaves; lastMultipleWaves = multipleWaves; lastAmplitude2 = amplitude2; lastFrequency2 = frequency2;
 
 	//For that creates wave vectors, frequency and amplitude for each of the total waves
@@ -114,7 +165,9 @@ void PhysicsInit() {
 	//Creation of all glm::vec3 arrays
 	nodeVectors = new glm::vec3[totalVertex];
 	initalVector = new glm::vec3[totalVertex];
-	sphereVector = { 0,5,0 };
+	closePos = new float[totalVertex];
+	sphereVector = startingVector;
+	sphereVelocity = { 0,0,0 };
 
 	//Sphere setup
 	Sphere::setupSphere(sphereVector, radius);
@@ -125,7 +178,7 @@ void PhysicsInit() {
 
 	//Applying values to "last" variables for reseting
 	lastTime = resetTime; lastHeight = height; lastAmplitude = amplitude; lastFrequency = frequency;
-	lastRadius = radius; lastMass = mass; lastDensity = density;
+	lastRadius = radius; lastMass = mass; lastDensity = density; lastSphereVector = startingVector;
 
 	//For that creates the Mesh with an "L" separation
 	for (int i = 0; i < totalVertex; i++) { 
@@ -135,15 +188,12 @@ void PhysicsInit() {
 		else { columnsCounter += 1; }
 	}
 
-	//Calculus of the nearest nodes to the sphere
-	for (int i = 0; i < totalVertex; i++) {
-		glm::distance(nodeVectors[i], sphereVector)
-
-	}
+	//Sphere calculus
+	getfluidHeight(10);
 }
 
 //Check variables changes to reset the simulation
-void checkChanges() { if (lastHeight != height || lastTime != resetTime || lastFrequency != frequency || lastAmplitude != amplitude  || lastMass != mass || lastRadius != radius  || lastDensity != density) PhysicsInit(); } 
+void checkChanges() { if (lastHeight != height || lastTime != resetTime || lastFrequency != frequency || lastAmplitude != amplitude  || lastMass != mass || lastRadius != radius  || lastDensity != density || lastSphereVector != startingVector) PhysicsInit(); }
 
 void checkWavesChanges() { if (lastTotalWaves != totalWaves || lastAmplitude2 != amplitude2 || lastFrequency2 != frequency2) initMultipleWaves(); }
 
@@ -188,8 +238,9 @@ void PhysicsUpdate(float dt) {
 		}
 	
 	//Calculus of external forces on a Sphere
-	sphereVector.y += -gravity * mass * dt;
-
+	sphereVelocity.y += -gravity * dt; //Apply gravity to the sphere
+	calculateBuoyancy(dt); //Calculate buoyancy force and apply it if necessary
+	sphereVector += sphereVelocity * dt;
 	if (dtCounter >= resetTime) {PhysicsInit(); dtCounter = 0; } //Reset every "x" seconds
 
 	ClothMesh::updateClothMesh(&nodeVectors[0].x); //Update the mesh positions
